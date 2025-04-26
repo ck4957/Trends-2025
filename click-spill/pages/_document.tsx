6977998e3c3a769bdd5bdcf3c0566356636bf692
Chirag Kular <ck4957@rit.edu>
2025-04-26 12:51:07 +0530

    Add Vercel's SpeedInsights

--- click-spill/pages/_document.tsx
+++ click-spill/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export default function Document() {
   return (
     <Html lang="en">
@@ -16,6 +17,7 @@ export default function Document() {
         </noscript>
         <Main />
         <NextScript />
+        <SpeedInsights />
         <Analytics />
       </body>
     </Html>
